247449c57cfea90ba9f9c180e5cb1170d84a190e
RhonSanchez <rhonaldsanchez98@gmail.com>
2023-05-12 22:17:23 -0400

    cambios al server

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 import jsonServer from "json-server";
-const server = jsonServer.create();
-const router = jsonServer.router('db.json');
-const middlewares = jsonServer.defaults();
+const server = jsonServer.create()
+const router = jsonServer.router('db.json')
+const middlewares = jsonServer.defaults()
 
-const PORT = process.env.PORT || 8000;
+server.use(middlewares)
+// Add this before server.use(router)
+server.use(jsonServer.rewriter({
+    '/api/*': '/$1',
+    '/products/:resource/:id/show': '/:resource/:id'
+}))
+server.use(router)
+server.listen(3000, () => {
+    console.log('JSON Server is running')
+})
 
-server.use(middlewares);
-server.use(router);
-
-server.listen(PORT, () => {
-  console.log(`JSON Server is running on port ${PORT}`);
-});
+// Export the Server API
+module.exports = server
